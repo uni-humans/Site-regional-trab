fba06c576443fab84632446c4611f6c7013f0a22
Alkidok <67169422+Alkidok@users.noreply.github.com>
2022-04-04 15:28:51 -0300

    eu panguei mt ontem
    ta salvo se quiser usar como modelo anyways

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //script
+//transição da barra de navegação
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
